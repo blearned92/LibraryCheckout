f6a6478c0d34ae4929628ceac7d2b81d5d82b4e8
Brandon Learned <blearned92@gmail.com>
2022-01-05 22:10:19 -0700

    Added some outputs

--- library.js
+++ library.js
@@ -74,34 +74,50 @@ class Movie extends Media {
 
 
 //general media
-const moulinRouge = new Media("Moulin Rouge!");
+const moulinRouge = new Movie("Moulin Rouge!", "Baz Luhrman", 127);
 const perfume = new Book("Perfume: Story of a Murderer", 
-"Patrick Suskind", 370);
-const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544)
-
+"Patrick Suskind", 263);
+const historyOfEverything = new Book('A Short History of Nearly Everything', 'Bill Bryson', 544)
+const speed = new Movie('Speed', 'Jan de Bont', 116)
 //books
-console.log("Title: " + perfume.title)
+console.log("Book Title: " + perfume.title)
 console.log("Author: " + perfume.author)
 console.log("Page Amount: " + perfume.pages)
-perfume.addRating(5)
+perfume.addRating(8.5)
 perfume.addRating(6)
 perfume.addRating(8)
 console.log("Average Book Rating: " + perfume.getAverageRating())
 perfume.toggleCheckoutStatus()
 console.log("Is the book checked out?: " + perfume.isCheckedOut)
 console.log("==============================")
-console.log("Title: " + historyOfEverything.title)
+console.log("Book Title: " + historyOfEverything.title)
 console.log("Author: " + historyOfEverything.author)
 console.log("Page Amount: " + historyOfEverything.pages)
 historyOfEverything.toggleCheckoutStatus()
 console.log("Is the book checked out?: " + historyOfEverything.isCheckedOut)
+historyOfEverything.addRating(3)
+historyOfEverything.addRating(10)
+historyOfEverything.addRating(2)
+console.log("Average Book Rating: " + historyOfEverything.getAverageRating())
 console.log("==============================")
 //Movies
 console.log("Movie Title: " + moulinRouge.title)
+console.log("Movie Run Time: " + moulinRouge.runTime)
+console.log("Movie Director: " + moulinRouge.director)
 console.log("Is the movie checked out? " + moulinRouge.isCheckedOut)
 moulinRouge.toggleCheckoutStatus()
 console.log("Is the movie checked out? " + moulinRouge.isCheckedOut)
-moulinRouge.addRating(5)
-moulinRouge.addRating(4)
+moulinRouge.addRating(10)
+moulinRouge.addRating(9.5)
 console.log("Average Movie Rating: " + moulinRouge.getAverageRating())
 console.log("==============================")
+console.log("Movie Title: " + speed.title)
+console.log("Movie Run Time: " + speed.runTime)
+console.log("Movie Director: " + speed.director)
+speed.toggleCheckoutStatus()
+console.log("Is the movie checked out? " + speed.isCheckedOut)
+speed.addRating(1)
+speed.addRating(5)
+speed.addRating(1)
+console.log("Average Movie Rating: " + speed.getAverageRating())
+
